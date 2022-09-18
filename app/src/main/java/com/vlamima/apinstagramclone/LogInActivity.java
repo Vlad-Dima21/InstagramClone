da98d0ceb584b0734190240c58582a7fdc10da29
Vlad-Dima21 <vladima2001@gmail.com>
2022-09-18 21:04:46 +0300

    Added keyboard dismiss on tap

--- app/src/main/java/com/vlamima/apinstagramclone/LogInActivity.java
+++ app/src/main/java/com/vlamima/apinstagramclone/LogInActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -49,6 +50,17 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnLogInLogin:
+                if (edtEmail.getText().toString().equals("") ||
+                        edtPassword.getText().toString().equals("")) {
+
+                    FancyToast.makeText(LogInActivity.this,
+                            "All fields are required",
+                            FancyToast.LENGTH_LONG,
+                            FancyToast.INFO,
+                            false).show();
+                    return;
+                }
+
                 ProgressDialog progressDialog = new ProgressDialog(this);
                 progressDialog.setMessage("Logging in");
                 progressDialog.show();
@@ -76,6 +88,17 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                 Intent intent = new Intent(this, SignUp.class);
                 startActivity(intent);
                 break;
+        }
+    }
+
+    public void rootLayoutTapped(View view) {
+        try {
+            InputMethodManager inputMethodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/vlamima/apinstagramclone/SignUp.java
+++ app/src/main/java/com/vlamima/apinstagramclone/SignUp.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -102,6 +103,17 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 Intent intent = new Intent(SignUp.this, LogInActivity.class);
                 startActivity(intent);
                 break;
+        }
+    }
+
+    public void rootLayoutTapped(View view) {
+        try {
+            InputMethodManager inputMethodManager =
+                    (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
