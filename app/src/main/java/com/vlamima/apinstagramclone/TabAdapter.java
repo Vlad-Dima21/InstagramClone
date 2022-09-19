628b24cd2d1a975d0d521f3ae0ac6ddc3af13109
Vlad-Dima21 <vladima2001@gmail.com>
2022-09-19 16:07:38 +0300

    Added Toolbar

---
+++ app/src/main/java/com/vlamima/apinstagramclone/TabAdapter.java
@@ -0,0 +1,45 @@
+package com.vlamima.apinstagramclone;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAdapter extends FragmentPagerAdapter {
+
+    public TabAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 1:
+                return new UsersTab();
+            case 2:
+                return new SharePictureTab();
+            default:
+                return new ProfileTab();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 1:
+                return "Users";
+            case 2:
+                return "Share Picture";
+            default:
+                return "Profile";
+        }
+    }
+}
